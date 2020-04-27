c7391a2dd81be8e0435ef1ca5bfe6519647164a9
Will Bashelor <wbashelor@gmail.com>
2020-04-27 14:26:38 -0400

    Create new config file

---
+++ test/specs/basic.js
@@ -0,0 +1,9 @@
+describe("webdriver.io page", () => {
+  it("should have the right title", () => {
+    browser.url("https://webdriver.io");
+    const title = browser.getTitle();
+    expect(browser).toHaveTitle(
+      "WebdriverIO · Next-gen browser automation test framework for Node.js"
+    );
+  });
+});
