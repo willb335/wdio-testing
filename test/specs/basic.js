e8bad713d26e0412b8c818264971e3db9ad44471
Will Bashelor <wbashelor@gmail.com>
2020-04-27 14:41:44 -0400

    Assert title and url

--- test/specs/basic.js
+++ test/specs/basic.js
@@ -1,9 +1,9 @@
-describe("webdriver.io page", () => {
-  it("should have the right title", () => {
-    browser.url("https://webdriver.io");
-    const title = browser.getTitle();
-    expect(browser).toHaveTitle(
-      "WebdriverIO · Next-gen browser automation test framework for Node.js"
-    );
-  });
-});
+// describe("webdriver.io page", () => {
+//   it("should have the right title", () => {
+//     browser.url("https://webdriver.io");
+//     const title = browser.getTitle();
+//     expect(browser).toHaveTitle(
+//       "WebdriverIO · Next-gen browser automation test framework for Node.js"
+//     );
+//   });
+// });
--- test/specs/example.js
+++ test/specs/example.js
@@ -1,5 +1,14 @@
 describe("First steps with WebdriverIO", () => {
   it("Loads example.com", () => {
     browser.url("http://www.example.com");
+    browser.pause(3000);
+
+    expect(browser).toHaveUrl("http://www.example.com/");
+    expect(browser).toHaveTitle("Example Domain");
   });
+
+  // it("Loads Webdriver's website", () => {
+  //   browser.url("https://webdriver.io");
+  //   browser.pause(3000);
+  // });
 });
