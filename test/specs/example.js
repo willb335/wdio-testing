810fbd0223b21b0391d44d46df4ac1f4ab4f3c5a
Will Bashelor <wbashelor@gmail.com>
2020-04-28 09:16:48 -0400

    Get element text

--- test/specs/example.js
+++ test/specs/example.js
@@ -9,19 +9,30 @@ describe("First steps with WebdriverIO", () => {
 
   it("Has a visible h1", () => {
     const h1 = $("h1");
+    h1.waitForExist();
 
     expect(h1).toBeVisible();
   });
 
   it("Has a visible p", () => {
     const p = $("p");
+    p.waitForExist();
 
     expect(p).toBeVisible();
   });
 
   it("Checks link value", () => {
     const link = $("a");
+    link.waitForExist();
 
     expect(link).toHaveLink("https://www.iana.org/domains/example");
+  });
+
+  it("Gets element text", () => {
+    const text = $("h1").getText();
+    const element = $("h1");
+
+    element.waitForExist();
+    expect(element).toHaveText("Example Domain");
   });
 });
