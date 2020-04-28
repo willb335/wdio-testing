422c5e93d1ef8c71fa6555b1da9bc53f5b4c2df6
Will Bashelor <wbashelor@gmail.com>
2020-04-28 14:59:19 -0400

    Log out

--- test/specs/e2e-login.js
+++ test/specs/e2e-login.js
@@ -2,20 +2,50 @@ const { short } = require('../../lib/timeouts');
 
 describe('E2E Tests - Login / Logout', () => {
   it('Does not login with invalid credentials', () => {
-    browser.url('http://zero.webappsecurity.com/index.html');
     const signInButton = $('#signin_button');
+    const logInForm = $('#login_form');
+    const userLogin = $('#user_login');
+    const userPassword = $('#user_password');
+    const submit = $('input[type="submit"]');
+    const navTabs = $('.nav-tabs');
+
+    browser.url('http://zero.webappsecurity.com/index.html');
     signInButton.waitForExist();
     signInButton.click();
-    const logInForm = $('#login_form');
     logInForm.waitForExist();
-    const userLogin = $('#user_login');
     userLogin.setValue('invalid');
-    const userPassword = $('#user_password');
     userPassword.setValue('invalid');
     browser.pause(short);
-    const submit = $('input[type="submit"]');
     submit.click();
     const error = $('.alert-error');
     expect(error).toHaveText('Login and/or password are wrong.');
+  });
+
+  it('Does login with valid credentials', () => {
+    const signInButton = $('#signin_button');
+    const logInForm = $('#login_form');
+    const userLogin = $('#user_login');
+    const userPassword = $('#user_password');
+    const submit = $('input[type="submit"]');
+    const navTabs = $('.nav-tabs');
+
+    browser.url('http://zero.webappsecurity.com/index.html');
+    signInButton.waitForExist();
+    signInButton.click();
+    logInForm.waitForExist();
+    userLogin.setValue('username');
+    userPassword.setValue('password');
+    browser.pause(short);
+    submit.click();
+    navTabs.waitForExist();
+  });
+
+  it('Logs out from app', () => {
+    $('.icon-user').waitForExist();
+    $('.icon-user').click();
+    $('#logout_link').waitForExist();
+    $('#logout_link').click();
+    browser.pause(short);
+    $('#pages-nav').waitForExist();
   });
 });
