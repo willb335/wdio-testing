4570ac9a83536ff135ec36ca2f2ee72fcc57d5d6
Will Bashelor <wbashelor@gmail.com>
2020-04-28 14:22:10 -0400

    Clicks radio and checkbox

--- test/specs/actions.js
+++ test/specs/actions.js
@@ -1,15 +1,34 @@
 const { short } = require('../../lib/timeouts');
 
 describe('Browser Actions', () => {
-  it('Inputs data', () => {
+  // it('Inputs data', () => {
+  //   browser.url('https://devexpress.github.io/testcafe/example/');
+  //   const input = $('#developer-name');
+  //   input.waitForExist();
+  //   input.setValue('Will');
+  //   browser.pause(short);
+  //   input.clearValue();
+  //   browser.pause(short);
+  //   input.addValue('Will');
+  //   browser.pause(short);
+  // });
+  //
+  // it('Clicks populate button', () => {
+  //   const populateButton = $('#populate');
+  //   populateButton.waitForExist();
+  //   populateButton.click();
+  // });
+
+  it('Clicks checkbox and radio button', () => {
     browser.url('https://devexpress.github.io/testcafe/example/');
-    const input = $('#developer-name');
-    input.waitForExist();
-    input.setValue('Will');
-    browser.pause(short);
-    input.clearValue();
-    browser.pause(short);
-    input.addValue('Will');
+    const radio = $('#linux');
+    radio.waitForExist();
+    radio.click();
+    radio.pause(short);
+
+    const checkbox = $('#remote-testing');
+    checkbox.waitForExist();
+    checkbox.click();
     browser.pause(short);
   });
 });
