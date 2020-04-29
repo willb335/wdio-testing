53ab58dc06d413d34a5fba69432984dd1a6b8423
Will Bashelor <wbashelor@gmail.com>
2020-04-29 18:09:51 -0400

    Refactor payee

--- page-objects/components/InsideNavbar.js
+++ page-objects/components/InsideNavbar.js
@@ -19,9 +19,34 @@ class InsideNavbar extends Base {
   get amount() {
     return $('#sp_amount');
   }
+  get date() {
+    return $('#sp_date');
+  }
+  get description() {
+    return $('#sp_description');
+  }
+  get paymentButton() {
+    return $('#pay_saved_payees');
+  }
+  get alertContent() {
+    return $('#alert_content');
+  }
+
+  clickPaymentButton() {
+    this.paymentButton.waitForExist();
+    this.paymentButton.click();
+  }
+  inputDescription(description) {
+    this.description.waitForExist();
+    this.description.setValue(description);
+  }
+  inputDate(date) {
+    this.date.waitForExist();
+    this.date.setValue(date);
+  }
   inputAmount(amount) {
     this.amount.waitForExist();
-    this.amount.setValue('500');
+    this.amount.setValue(amount);
   }
   selectAccount(account) {
     this.accountSelection.waitForExist();
--- test/specs/e2e-pay.js
+++ test/specs/e2e-pay.js
@@ -1,6 +1,7 @@
 import App from '../../page-objects/App';
 import LoginPage from '../../page-objects/pages/LoginPage';
 import Navbar from '../../page-objects/components/Navbar';
+import InsideNavbar from '../../page-objects/components/InsideNavbar';
 
 describe('E2E Tests - Pay', () => {
   it('Should log into application', () => {
@@ -10,18 +11,28 @@ describe('E2E Tests - Pay', () => {
   });
 
   it('Should make payment', () => {
-    $('#pay_bills_tab').click();
-    const selectPayee = $('#sp_payee');
-    selectPayee.waitForExist();
-    selectPayee.selectByAttribute('value', 'apple');
-    const selectAccount = $('#sp_account');
-    selectAccount.waitForExist();
-    selectAccount.selectByVisibleText('Loan');
-    $('#sp_amount').setValue('500');
-    $('#sp_date').setValue('2020-03-31');
-    $('#sp_description').setValue('Test');
-    $('#pay_saved_payees').click();
-    const message = $('#alert_content');
-    expect(message).toHaveText('The payment was successfully submitted.');
+    InsideNavbar.clickPayBillsTab();
+    InsideNavbar.selectPayee('apple');
+    InsideNavbar.selectAccount('Loan');
+    InsideNavbar.inputAmount('500');
+    InsideNavbar.inputDate('2020-03-31');
+    InsideNavbar.inputDescription('Test');
+    InsideNavbar.clickPaymentButton();
+    // $('#pay_bills_tab').click();
+    // const selectPayee = $('#sp_payee');
+    // selectPayee.waitForExist();
+    // selectPayee.selectByAttribute('value', 'apple');
+
+    // const selectAccount = $('#sp_account');
+    // selectAccount.waitForExist();
+    // selectAccount.selectByVisibleText('Loan');
+    // $('#sp_amount').setValue('500');
+    // $('#sp_date').setValue('2020-03-31');
+    // $('#sp_description').setValue('Test');
+    // $('#pay_saved_payees').click();
+    // const message = $('#alert_content');
+    expect(InsideNavbar.alertContent).toHaveText(
+      'The payment was successfully submitted.'
+    );
   });
 });
