5708a6415e5759028828ae1157a484db0b9ac977
Will Bashelor <wbashelor@gmail.com>
2020-04-30 08:31:56 -0400

    Refactor foreign currency page

--- page-objects/components/InsideNavbar.js
+++ page-objects/components/InsideNavbar.js
@@ -10,6 +10,22 @@ class InsideNavbar extends Base {
   get payBillsTab() {
     return $('#pay_bills_tab');
   }
+  get purchaseForeignCurrencyTab() {
+    return $('#tabs > ul > li:nth-child(3) > a');
+  }
+  get currencySelection() {
+    return $('#pc_currency');
+  }
+  get currencyAmount() {
+    return $('#pc_amount');
+  }
+  get denominateInDollars() {
+    return $('#pc_inDollars_true');
+  }
+  get purchaseCash() {
+    return $('#purchase_cash');
+  }
+
   get payeeSelection() {
     return $('#sp_payee');
   }
@@ -19,6 +35,7 @@ class InsideNavbar extends Base {
   get amount() {
     return $('#sp_amount');
   }
+
   get date() {
     return $('#sp_date');
   }
@@ -48,6 +65,10 @@ class InsideNavbar extends Base {
     this.amount.waitForExist();
     this.amount.setValue(amount);
   }
+  inputCurrencyAmount(amount) {
+    this.currencyAmount.waitForExist();
+    this.currencyAmount.setValue(amount);
+  }
   selectAccount(account) {
     this.accountSelection.waitForExist();
     this.accountSelection.selectByVisibleText(account);
@@ -56,17 +77,38 @@ class InsideNavbar extends Base {
     this.payeeSelection.waitForExist();
     this.payeeSelection.selectByAttribute('value', payee);
   }
+
+  selectCurrency(currency) {
+    this.currencySelection.waitForExist();
+    this.currencySelection.selectByAttribute('value', currency);
+  }
+
   clickPayBillsTab() {
     this.payBillsTab.waitForExist();
     this.payBillsTab.click();
   }
+
+  clickPurchaseForeignCurrencyTab() {
+    this.purchaseForeignCurrencyTab.waitForExist();
+    this.purchaseForeignCurrencyTab.click();
+  }
+
   clickAccountActivityTab() {
     this.accountActivityTab.waitForExist();
     this.accountActivityTab.click();
   }
+
   clickFiltersLink() {
     this.filtersLink.waitForExist();
     this.filtersLink.click();
+  }
+  clickDenominateInDollars() {
+    this.denominateInDollars.waitForExist();
+    this.denominateInDollars.click();
+  }
+  clickPurchaseCash() {
+    this.purchaseCash.waitForExist();
+    this.purchaseCash.click();
   }
 }
 
--- test/specs/e2e-exchange.js
+++ test/specs/e2e-exchange.js
@@ -1,6 +1,7 @@
 import App from '../../page-objects/App';
 import LoginPage from '../../page-objects/pages/LoginPage';
 import Navbar from '../../page-objects/components/Navbar';
+import InsideNavbar from '../../page-objects/components/InsideNavbar';
 
 describe('E2E Tests - Currency Exchange', () => {
   it('Should log into application', () => {
@@ -10,18 +11,24 @@ describe('E2E Tests - Currency Exchange', () => {
   });
 
   it('Should make currency exchange', () => {
-    $('#pay_bills_tab').waitForExist();
-    $('#pay_bills_tab').click();
-    $('#tabs > ul > li:nth-child(3) > a').waitForExist();
-    $('#tabs > ul > li:nth-child(3) > a').click();
-    const currencySelect = $('#pc_currency');
-    currencySelect.waitForExist();
-    currencySelect.selectByAttribute('value', 'GBP');
-    $('#pc_amount').setValue('500');
-    $('#pc_inDollars_true').click();
-    $('#purchase_cash').click();
-    const message = $('#alert_content');
-    expect(message).toHaveText(
+    // $('#pay_bills_tab').waitForExist();
+    // $('#pay_bills_tab').click();
+    InsideNavbar.clickPayBillsTab();
+    // $('#tabs > ul > li:nth-child(3) > a').waitForExist();
+    // $('#tabs > ul > li:nth-child(3) > a').click();
+    InsideNavbar.clickPurchaseForeignCurrencyTab();
+    // const currencySelect = $('#pc_currency');
+    // currencySelect.waitForExist();
+    // currencySelect.selectByAttribute('value', 'GBP');
+    InsideNavbar.selectCurrency('GBP');
+    // $('#pc_amount').setValue('500');
+    InsideNavbar.inputCurrencyAmount('500');
+    // $('#pc_inDollars_true').click();
+    InsideNavbar.clickDenominateInDollars();
+    // $('#purchase_cash').click();
+    InsideNavbar.clickPurchaseCash();
+    // const message = $('#alert_content');
+    expect(InsideNavbar.alertContent).toHaveText(
       'Foreign currency cash was successfully purchased.'
     );
   });
