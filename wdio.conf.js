15330abb284ba4472e772157a1c731b2a9bc7f1a
Will Bashelor <wbashelor@gmail.com>
2020-04-30 08:47:08 -0400

    Set up config file

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,5 @@
+const config = require('./lib/config');
+
 exports.config = {
   //
   // ====================
@@ -35,7 +37,7 @@ exports.config = {
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
   //
-  maxInstances: 10,
+  maxInstances: config.maxInstances,
   //
   // If you have trouble getting all important capabilities together, check out the
   // Sauce Labs platform configurator - a great tool to configure your capabilities:
@@ -48,7 +50,7 @@ exports.config = {
       // 5 instances get started at a time.
       maxInstances: 5,
       //
-      browserName: 'chrome',
+      browserName: config.browser,
       // If outputDir is provided WebdriverIO can capture driver session logs
       // it is possible to configure which logTypes to include/exclude.
       // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
@@ -62,7 +64,7 @@ exports.config = {
   // Define all options that are relevant for the WebdriverIO instance here
   //
   // Level of logging verbosity: trace | debug | info | warn | error | silent
-  logLevel: 'info',
+  logLevel: config.logLevel,
   //
   // Set specific log levels per logger
   // loggers:
@@ -80,16 +82,16 @@ exports.config = {
   //
   // If you only want to run your tests until a specific amount of tests have failed use
   // bail (default is 0 - don't bail, run all tests).
-  bail: 0,
+  bail: config.bail,
   //
   // Set a base URL in order to shorten url command calls. If your `url` parameter starts
   // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
   // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
   // gets prepended directly.
-  baseUrl: 'http://localhost',
+  baseUrl: config.baseUrl,
   //
   // Default timeout for all waitFor* commands.
-  waitforTimeout: 10000,
+  waitforTimeout: config.timeout,
   //
   // Default timeout in milliseconds for request
   // if browser driver or grid doesn't send response
